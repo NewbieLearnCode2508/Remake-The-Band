d9519478ecc6d6329c057e4eb27ba49cb95da506
NewbieLearnCode2508 <trantiendat25082003@gmail.com>
2022-02-16 22:47:30 +0700

    upload more file

--- app.js
+++ app.js
@@ -5,8 +5,20 @@ const modal = $('.modal');
 const modalClose = $('.modal-close');
 const buyBtn = $$('.booking-buy-btn');
 const modalContainer = $('.modal-container');
+const slideImg = $('.slider-img');
 
 const app = {
+    silder: [
+        {
+            imgSrc: "https://www.w3schools.com/w3images/la.jpg"
+        },
+        {
+            imgSrc: "https://www.w3schools.com/w3images/ny.jpg"
+        },
+        {
+            imgSrc: "https://www.w3schools.com/w3images/chicago.jpg"
+        }
+    ],
     handleEvent: function() {
         buyBtn.forEach((e)=> {
             e.onclick = ()=> {
@@ -21,6 +33,14 @@ const app = {
                 modal.classList.remove('open');
             }
         }
+        let idx = 0;
+        setInterval(()=> {
+            slideImg.src = this.silder[idx].imgSrc;
+            idx++;
+            if(idx == this.silder.length) {
+                idx = 0;
+            }
+        }, 4000)
     },
     start: function() {
         this.handleEvent();
